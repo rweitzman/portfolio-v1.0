9d95aa8da81a374f6814826824ed7cc51b1cfd09
wsushinsky <wyattsushi@gmail.com>
2021-08-26 14:46:15 -0700

    added email functionality

--- server.js
+++ server.js
@@ -1,14 +1,55 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
+const nodemailer = require("nodemailer");
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 const favicon = require('serve-favicon');
-app.use(favicon(__dirname + '/public/img/robin.png'));
+app.use(favicon(__dirname + "/public/img/robin.png"));
 
-app.get('/', function (req, res) {
-  res.render('index');
+app.get("/", function (req, res) {
+  let message = req.query.message;
+  if (!message) message = "none";
+
+  res.render("index", {
+    message: message
+  });
+});
+
+app.post("/message", async function (req, res) {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.NOREPLYUSER,
+      pass: process.env.NOREPLYPW
+    }
+  });
+    
+  var mailOptions = {
+    from: process.env.NOREPLYUSER,
+    to: "wyattsushi@gmail.com",
+    subject: 'Message from your Portfolio Page!!!',
+    html: `<h2>Message from your Portfolio Page:<h2><br><br>` +
+      `<h3>${req.body.name}</h3><br>` +
+      `<h4>${req.body.email}</h4><br>` + 
+      `<p>${req.body.message}</p><br><br><br><br>` + 
+      `<p>Good luck and stay safe! - Wyatt</p>`,
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+
+      res.redirect("/?message=fail#contact");
+    } 
+    else {
+      res.redirect("/?message=success#contact");
+    }
+  });
+  
 });
 
 app.listen(3000, () => {
